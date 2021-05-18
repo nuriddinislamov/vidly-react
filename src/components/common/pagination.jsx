f13ea710d1bc278f3df0664dfabc1e932bf126a1
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-05-18 20:16:07 +0500

    Extracting a Table component

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -4,7 +4,6 @@ import _ from 'lodash';
 
 const Pagination = (props) => {
     const { itemsCount, pageSize, currentPage, onPageChange } = props;
-    console.log(currentPage);
     
     const pagesCount = Math.ceil(itemsCount / pageSize);
     const pages = _.range(1, pagesCount + 1);
@@ -15,7 +14,7 @@ const Pagination = (props) => {
         <ul className="pagination">
             { pages.map(page => (
                 <li key={page} className={ page === currentPage ? 'page-item active' : "page-item"}>
-                <a onClick={()=> onPageChange(page)} href="#" className="page-link">{page}</a>
+                <a onClick={()=> onPageChange(page)} className="page-link">{page}</a>
             </li>
             ))}
             
--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Like from './common/like';
-import TableHeader from "./common/tableHeader";
+import Table from "./common/table";
+
 
 class MoviesTable extends Component {
     columns = [
@@ -8,35 +9,23 @@ class MoviesTable extends Component {
         { path: 'genre.name', label: "Genre" },
         { path: 'numberInStock', label: "Stock" },
         { path: 'dailyRentalRate', label: "Rate" },
-        { key: 'like'},
-        { key: 'delete'}
+        { key: 'like', content: movie =>
+        <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />},
+        { key: 'delete', content: movie => (
+        <button onClick={() => this.props.onDelete(movie)} className="btn btn-danger btn-sm">
+        Delete</button>)}
     ]
     
     render() { 
-        const { movies, onDelete, onLike, onSort, sortColumn } = this.props;
-        return ( <table className="table">
-        <TableHeader
+        const { movies, onSort, sortColumn } = this.props;
+        return ( 
+        <Table
         columns={this.columns}
+        data={movies}
         sortColumn={sortColumn}
-        onSort={onSort}/>
-        <TableBody data={movies}/>
-        <tbody>
-            { movies.map(movie => (
-            <tr key={movie._id}>
-                <td>{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <td>
-                    <Like liked={movie.liked}
-                    onClick={() => onLike(movie)} />
-                </td>
-                <td><button onClick={() => onDelete(movie)} className="btn btn-danger btn-sm">
-                    Delete</button></td>
-            </tr>
-            ))}
-        </tbody>
-    </table> );
+        onSort={onSort}
+        /> 
+        );
     }
 }
  
